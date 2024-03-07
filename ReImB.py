bc12b2b5d9be8949486eb9013be0294223e6d88e
n.hagi0806 <iwgpoki6755@gmail.com>
2024-03-07 14:01:16 +0900

    commit

---
+++ ReImB.py
@@ -0,0 +1,39 @@
+import math
+import numpy as np
+import const
+import pandas as pd
+
+def Gamma(E):
+    gnGamma_0 = const.gnGamma_0*np.sqrt(E/np.abs(const.E_0))
+    gnGamma_1 = const.gnGamma_1*np.sqrt(E/const.E_1)
+
+    nGamma_0 = gnGamma_0/(2*const.g0)
+    nGamma_1 = gnGamma_1/(2*const.g1)
+
+    Gamma_0 = nGamma_0 + const.Gammagamma_0
+    Gamma_1 = nGamma_1 + const.Gammagamma_1
+
+    return nGamma_0, nGamma_1, Gamma_0, Gamma_1
+
+
+def Gamma131(E):
+    gnGamma_0 = const.gnGamma_0*np.sqrt(E)
+    gnGamma_1 = const.gnGamma_1*np.sqrt(E/const.E_1)
+    gnGamma_2 = const.gnGamma_2*np.sqrt(E/const.E_2)
+
+    nGamma_0 = gnGamma_0/(2*const.g0)
+    nGamma_1 = gnGamma_1/(2*const.g1)
+    nGamma_2 = gnGamma_2/(2*const.g1)
+
+    Gamma_0 = nGamma_0 + const.Gammagamma_0
+    Gamma_1 = nGamma_1 + const.Gammagamma_1
+    Gamma_2 = nGamma_2 + const.Gammagamma_2
+
+    return nGamma_0, nGamma_1, nGamma_2, Gamma_0, Gamma_1, Gamma_2
+
+
+def ImB_131Xe(E):
+    nGamma_0, nGamma_1, nGamma_2, Gamma_0, Gamma_1, Gamma_2 = Gamma131(E)
+    k = 0.6947*np.sqrt(E*10**3)*10**10
+    
+    return 3/(64*k)*(((4*nGamma_2*(2*k*(E-const.E_2)*const.R_0-(Gamma_2/2)))/((E-const.E_2)**2+(Gamma_2/2)**2))-((4*nGamma_0*(2*k*(E-const.E_0)*const.R_0-(Gamma_0/2)))/((E-const.E_0)**2+(Gamma_0/2)**2)))+3/(64*k)*((7*nGamma_1*Gamma_1)/(2*((E-const.E_1)**2+(Gamma_1/2)**2)))
---
+++ graph.py
@@ -0,0 +1,20 @@
+import math
+import numpy as np
+import const
+import pandas as pd
+import ReImB.py
+import matplotlib.pyplot as plt
+
+# グラフを描画する範囲を設定
+E_values = np.linspace(0, 1000, 100000)  # 0から10までの間を100点で分割して計算
+
+# 関数を適用し、結果を取得
+ImB_values = [ImB_131Xe(E) for E in E_values]
+
+# グラフを描画
+plt.plot(E_values, ImB_values)
+plt.xlabel('E')  # x軸のラベル
+plt.ylabel('ImB_131Xe(E)')  # y軸のラベル
+plt.title('ImB_131Xe(E) vs E')  # グラフのタイトル
+plt.grid(True)  # グリッドを表示
+plt.show()  # グラフを表示
