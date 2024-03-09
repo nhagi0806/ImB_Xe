9793deef0345ab44f52fb00d0a2f3840c8fc5dd1
n.hagi0806 <iwgpoki6755@gmail.com>
2024-03-09 16:15:44 +0900

    comitt

--- ReImB.py
+++ ReImB.py
@@ -37,3 +37,6 @@ def ImB_131Xe(E):
     k = 0.6947*np.sqrt(E*10**3)*10**10
     
     return 3/(64*k)*(((4*nGamma_2*(2*k*(E-const.E_2)*const.R_0-(Gamma_2/2)))/((E-const.E_2)**2+(Gamma_2/2)**2))-((4*nGamma_0*(2*k*(E-const.E_0)*const.R_0-(Gamma_0/2)))/((E-const.E_0)**2+(Gamma_0/2)**2)))+3/(64*k)*((7*nGamma_1*Gamma_1)/(2*((E-const.E_1)**2+(Gamma_1/2)**2)))
+
+def ImB_129(E):
+    
--- graph.py
+++ graph.py
@@ -2,14 +2,14 @@ import math
 import numpy as np
 import const
 import pandas as pd
-import ReImB.py
+import ReImB
 import matplotlib.pyplot as plt
 
 # グラフを描画する範囲を設定
-E_values = np.linspace(0, 1000, 100000)  # 0から10までの間を100点で分割して計算
+E_values = np.linspace(0, 50, 100000)  # 0から10までの間を100点で分割して計算
 
 # 関数を適用し、結果を取得
-ImB_values = [ImB_131Xe(E) for E in E_values]
+ImB_values = [ReImB.ImB_131Xe(E) for E in E_values]
 
 # グラフを描画
 plt.plot(E_values, ImB_values)
